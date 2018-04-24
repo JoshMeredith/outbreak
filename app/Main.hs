1ca0f5542ce2938558e5cbf1701be31e1a477439
Josh Meredith <joshmeredith2008@gmail.com>
2018-04-25 02:32:40 +1000

    Rewrite flow logic into (currently unused) top level functions

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE FlexibleContexts    #-}
 {-# LANGUAGE BangPatterns        #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns        #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE RankNTypes          #-}
 
 import Data.Array.Accelerate
 import Data.Array.Accelerate.LLVM.Native as CPU
@@ -33,9 +36,9 @@ maxSteps = 50
 
 
 main = defaultMain
-         [ bench "CPU/iterate" $ whnf (\p -> P.iterate goC p P.!! maxSteps) population
-         , bench "CPU/acc"     $ whnf (CPU.runN outbreakN) population
-         , bench "PTX/iterate" $ whnf (\p -> P.iterate goP p P.!! maxSteps) population
+         [ --bench "CPU/iterate" $ whnf (\p -> P.iterate goC p P.!! maxSteps) population
+         -- , bench "CPU/acc"     $ whnf (CPU.runN outbreakN) population
+         bench "PTX/iterate" $ whnf (\p -> P.iterate goP p P.!! maxSteps) population
          , bench "PTX/acc"     $ whnf (PTX.runN outbreakN) population
          ]
 -- main = P.print . CPU.run . sums . outbreak $ population
@@ -44,10 +47,6 @@ main = defaultMain
 
     !goP  = PTX.runN outbreak
     !goC  = CPU.runN outbreak
-    steps = P.iterate go population
-
-    final = steps P.!! maxSteps
-
 
     outbreakN :: Acc (Vector SPop, Vector IPop, Vector RPop)
               -> Acc (Vector SPop, Vector IPop, Vector RPop)
@@ -107,7 +106,7 @@ stepOutbreak transmissions recoveries quarantines arrivals departures population
   lift (susceptible', infected', recovered')
   where
     (susceptible, infected, recovered) = unlift populations
-    totals = zipWith3 (\s i r -> s + i + r) susceptible infected recovered
+    totals = zipWith3 (((+) .) . (+)) susceptible infected recovered
     --
     newlyInfected :: Acc (Vector IPop)
     newlyInfected = zipWith4 infect transmissions susceptible infected totals
@@ -123,23 +122,11 @@ stepOutbreak transmissions recoveries quarantines arrivals departures population
     recover rate i = floor $ rate * fromIntegral i
     --
     (quarantined, notQuarantined) = unzip $ zipWith quarantine iArrivals quarantines
-    quarantine :: Exp IPop
-               -> Exp QuarantineRate
-               -> Exp (RPop, IPop)
-    quarantine i rate = let q = ceiling $ rate * fromIntegral i in lift (q, i - q)
     --
-    proportional :: Exp Int -> Exp Int -> Exp Int -> Exp Int
-    proportional compartment n flow = floor passengers
-      where
-        passengers :: Exp Float
-        passengers = (fromIntegral compartment / fromIntegral n) * fromIntegral flow
-    --
-    -- arrivals     = sum flows
     sArrivals    = zipWith3 proportional susceptible totals arrivals
     iArrivals    = zipWith3 proportional infected    totals arrivals
     rArrivals    = zipWith3 proportional recovered   totals arrivals
     --
-    -- departures   = sum . transpose $ flows
     sDepartures  = zipWith3 proportional susceptible totals departures
     iDepartures  = zipWith3 proportional infected    totals departures
     rDepartures  = zipWith3 proportional recovered   totals departures
@@ -153,3 +140,65 @@ stepOutbreak transmissions recoveries quarantines arrivals departures population
     recovered'   = zipWith5 stepR recovered newlyRecovered rArrivals rDepartures quarantined
     stepR r newr a d q = r + newr + a - d + q
 
+
+proportional :: Exp Int -> Exp Int -> Exp Int -> Exp Int
+proportional compartment n flow = floor passengers
+  where
+    passengers :: Exp Float
+    passengers = (fromIntegral compartment / fromIntegral n) * fromIntegral flow
+
+
+travelFlows
+  :: (forall a. Acc a -> a)              -- Run function for precomputation
+  -> Acc (Array DIM2 PassengerFlow)      -- Flows data matrix, departure major
+  -> Acc (Vector  (SPop, IPop, RPop))    -- Populations in each compartment for each city
+  -> Acc (Vector ((SPop, IPop, RPop)     -- Arrival   flows for each city
+                 ,(SPop, IPop, RPop)))   -- Departure flows for each city
+travelFlows run flows populations = zip aFlows dFlows
+  where
+    flowsT = use . run $ transpose flows
+    depts  = use . run $ sum       flows
+    coeffs = map goX populations
+    aFlows = arrivals   flowsT coeffs
+    dFlows = departures depts  coeffs
+    --
+    goX (unlift -> (s :: Exp Int, i :: Exp Int, r :: Exp Int)) = lift (ix, rx)
+      where
+        n  = fromIntegral (s + i + r)
+        ix = fromIntegral i / n
+        rx = fromIntegral r / n
+
+
+departures
+  :: Acc (Vector PassengerFlow)
+  -> Acc (Vector (Float, Float))
+  -> Acc (Vector (SPop, IPop, RPop))
+departures = zipWith go
+  where
+    go :: Exp PassengerFlow -> Exp (Float, Float) -> Exp (SPop, IPop, RPop)
+    go flow (unlift -> (ix :: Exp Float, rx :: Exp Float)) = lift (s, i, r)
+      where
+        s = flow - (i + r)
+        i = ceiling $ ix * fromIntegral flow
+        r = ceiling $ rx * fromIntegral flow
+
+
+arrivals
+  :: Acc (Array DIM2 PassengerFlow)    -- Flows data matrix, arrival major
+  -> Acc (Vector (Float, Float))
+  -> Acc (Vector (SPop, IPop, RPop))
+arrivals flows coeffs = zip3 (sum sFlows) (sum iFlows) (sum rFlows)
+  where
+    sFlows = zipWith3 (((-) .) . (-)) flows iFlows rFlows
+    iFlows = imap (go ixs) flows
+    rFlows = imap (go rxs) flows
+    --
+    (ixs, rxs) = unzip coeffs
+    --
+    go xs (indexHead -> from) flow = ceiling $ fromIntegral flow * (xs !! from)
+
+
+quarantine :: Exp IPop -> Exp QuarantineRate -> Exp (RPop, IPop)
+quarantine i rate = lift (q, i - q)
+  where
+    q = ceiling $ rate * fromIntegral i
